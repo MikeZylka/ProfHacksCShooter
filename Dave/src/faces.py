7c3ccf73e07a24097235e1dc17def90e1ab6307a
Mike <mikezylka123@live.com>
2020-03-01 03:03:04 -0500

    finished x calculation, working on y calculation

--- Dave/src/faces.py
+++ Dave/src/faces.py
@@ -1,18 +1,23 @@
 import numpy as np
 import cv2
+from datetime import *
 
 face_cascade = cv2.CascadeClassifier('C:/Users/mikez/Documents/Git/Hackathons/ProfHacks/Dave/src/cascades/data/haarcascade_frontalface_alt2.xml')
 
 cap = cv2.VideoCapture(1)
+sTime = datetime.now().second
+dAve = []
+width = int(cap.get(3))
+height = int(cap.get(4))
 
 while(True):
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors = 5)
-
+    cTime = datetime.now().second
     for (x,y,w,h) in faces:
-        print(x,y,w,h)
-
+        #print(x,y,w,h)
+        dAve = dAve + [(8.5 * 575.0034602076125)/ w]
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y-60:y+h+60, x-20:x+w+20]
 
@@ -21,6 +26,23 @@ while(True):
         x1 = x + w
         y1 = y + h
         cv2.rectangle(img, (x,y), (x1, y1), color, thickness)
+        cv2.rectangle(img, (width//2, 0), (width//2, height), (0,0,255), 2)
+        cv2.rectangle(img, (0,height//2), (width, height//2), (0,0,255), 2)
+
+        if (sTime != cTime):
+            sum = 0
+            for i in dAve:
+                sum = sum + i
+            D = sum / len(dAve)
+            #print(D)
+            dAve = []
+            sTime = datetime.now().second
+            cTime = datetime.now().second
+
+        cv2.putText(img, f"Distance: {D}", (x+(w//3), y+h-(h//3)), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1)
+        
+
+        cv2.rectangle(img, (x+(w//3), y+h-(h//3)), (x+w-(w//3), y+h+(h//7)), (0,255,0), 2)
         
         #img_item = "data.png"
         #cv2.imwrite(img_item, roi_color)
--- Dave/src/openMouth_train.py
+++ Dave/src/openMouth_train.py
@@ -1,10 +1,48 @@
 import os
+import cv2
+import numpy as np
+from PIL import Image
+import pickle
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images/open-mouth")
+
+
+face_cascade = cv2.CascadeClassifier('C:/Users/mikez/Documents/Git/Hackathons/ProfHacks/Dave/src/cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFa
+
+
+current_id = 0
+label_ids = {}
+y_labels = []
+x_train = []
 
 for root, dirs, files in os.walk(image_dir):
     for file in files:
         if file.endswith("png"):
             path = os.path.join(root, file)
             print(path)
+            label = "open-mouth"
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
+                
+            id_ = label_ids[label]
+            print(label_ids)
+
+            pil_image = Image.open(path).convert("L")
+            image_array = np.array(pil_image, "uint8")
+            print(image_array)
+            #faces = face_cascade.detectMultiScale(image_array)
+
+            x_train.append(image_array)
+            y_labels.append(id_)
+
+#print(y_labels)
+#print(x_train)
+
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids, f)
+
+
+recognizer.save("trainner.yml")
